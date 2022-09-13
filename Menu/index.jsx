774555840a48c1fcf4faa43c995ed89614796601
aXlireza <devops@axoncodes.com>
2022-09-13 22:52:06 +0430

    init menu copied from reaxfont components

---
+++ Menu/index.jsx
@@ -0,0 +1,38 @@
+export default function Menu({menuItems}) {
+  return (
+    <axg-element
+      mode="dropdown"
+      headTitlecolor="#FFF4A3"
+      height="70"
+      color="#282A35"
+      colorHover="#fff"
+      activeBackground="#282A35"
+      headBackground="#0000"
+      headBackgroundHover="#04AA6D"
+      structure="dropdownGroup"
+      title="Menu"
+      background="#cbcbcb"
+      subOpening="sub"
+      subTrigger="click"
+      dropdownid="headerMenuGroup"
+    >
+      {menuItems.map((item, key) => (
+        <axg-element
+          key={key}
+          mode='dropdown'
+          height="50"
+          color='var(--darkColor)'
+          colorHover='#000'
+          activeBackground='var(--darkColor)'
+          headBackground='#0000'
+          headBackgroundHover='var(--bg3)'
+          structure='link'
+          title={item.name}
+          link={item.uri}
+          breakpoint="700"
+          dropdownid={key}
+        ></axg-element>
+      ))}
+    </axg-element>
+  )
+}
