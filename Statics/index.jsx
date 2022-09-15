3786733835589afbebaf359f1a6d205afc26a522
aXlireza <devops@axoncodes.com>
2022-09-15 05:11:22 +0430

    add inlineStyle options and setting direction for the list items

--- Statics/index.jsx
+++ Statics/index.jsx
@@ -1,12 +1,12 @@
 import style from './style.module.css'
 
-export default function Statics({ globalClass=[], customClass=[], mode='flex', data }) {
+export default function Statics({ globalClass=[], customClass=[], mode='flex', data, inlineStyle }) {
   return (
-    <ul className={`${globalClass.join(' ')} ${customClass.map(cclass => style[cclass])} ${style[mode]} subcontainer center`}>
+    <ul style={inlineStyle} className={`${globalClass.join(' ')} ${customClass.map(cclass => style[cclass])} ${style[mode]} subcontainer center`}>
       {data.map((item, key) => (
         <li key={key}>
-          <span className={item[2] ? style.name : style.number}>{item[0]}</span>
-          <span className={item[2] ? style.number : style.name}>{item[1]}</span>
+          <span className={item[2] ? style.name : style.number} style={{direction: item[3] || 'ltr'}}>{item[0]}</span>
+          <span className={item[2] ? style.number : style.name} style={{direction: item[3] || 'ltr'}}>{item[1]}</span>
         </li>
       ))}
     </ul>
