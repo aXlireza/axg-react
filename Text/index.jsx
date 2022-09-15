4e3d84e84a1a2c28f4e8cc9eaee0e65d0729392a
aXlireza <devops@axoncodes.com>
2022-09-15 05:12:15 +0430

    add inlineStyle option

--- Text/index.jsx
+++ Text/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import style from './style.module.css'
 
-export default function Text({ size, align, children, tag='h1', color='var(--dark)', text='', customClass=[''], nomargin=0 }) {
+export default function Text({ size, align, children, tag='h1', color='var(--dark)', text='', customClass=[''], inlineStyle, nomargin=0 }) {
   return React.createElement(
     tag,
     {
       className: `${style.default} ${customClass.map(cclass => style[cclass]).join(' ')} ${nomargin ? style.nomargin : ''}`,
-      style: {color: color, textAlign: align, fontSize: size}
+      style: {...inlineStyle, color: color, textAlign: align, fontSize: size}
     },
     children||text
   )
