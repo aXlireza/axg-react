f792cdee0d19a2f2c255ffc71fca0c2fdd9d0b6e
aXlireza <devops@axoncodes.com>
2022-09-15 05:11:45 +0430

    use var colors

--- SubHeader/index.jsx
+++ SubHeader/index.jsx
@@ -3,10 +3,10 @@ import Text from '../Text'
 
 export default function SubHeader({top, mainTitle, description}) {
   return (
-    <section className='container'>
-      <Text text={top} nomargin={1} color={"#565969"} size={"var(--standard-text-fontSize)"} />
-      <Text text={mainTitle} nomargin={1} color={"#001659"} size={"var(--xLarge-text-fontSize)"} />
-      <Text text={description} nomargin={1} color={"#565969"} size={"var(--smallLarge-text-fontSize)"} />
+    <section className='container vertical righty' style={{rowGap: '0px', backgroundColor: 'var(--tertiaryColor)'}}>
+      <Text text={top} nomargin={1} color={"var(--secondaryTextColor)"} size={"var(--standard-text-fontSize)"} />
+      <Text text={mainTitle} nomargin={1} color={"var(--primaryTextColor)"} size={"var(--xLarge-text-fontSize)"} inlineStyle={{marginTop: `-1.8%`, marginBottom: '1%'}} />
+      <Text text={description} nomargin={1} color={"var(--secondaryTextColor)"} size={"var(--smallLarge-text-fontSize)"} />
     </section>
   )
 }
