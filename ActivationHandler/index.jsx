d34f48347ba80eb8f277863f23244f98eb3fa878
Axoncodes <devops@axoncodes.com>
2022-09-22 11:01:31 +0330

    use local axg

--- ActivationHandler/index.jsx
+++ ActivationHandler/index.jsx
@@ -3,7 +3,7 @@ import { loadScript, axgCdnHAndler } from "../../lib/script"
 
 export default function ActivationHandler() {
   useEffect(() => {
-    loadScript(`${process.env.Axg}/layoutactivationJs`)
+    loadScript(`${process.env.LocalAxg}/layoutactivationJs`)
     .then(() => activationHandler.init())
   }, [])
   return '';
