c2207a39a8cead420a75fe1d60408f94ce34fb70
Axoncodes <devops@axoncodes.com>
2022-09-22 11:01:01 +0330

    add onclicks

--- Button/index.jsx
+++ Button/index.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import Icon from '../Icon'
 import style from './style.module.css'
 
-export default function Button({ iconSize, icon, height="100%", text='Button', link='/', bg='var(--blue)', color="", size='normal', mode='button'} = {}) {
+export default function Button({ onClick, iconSize, icon, height="100%", text='Button', link='/', bg='var(--blue)', color="", size='normal', mode='button'} = {}) {
   const inner = (
     <div style={{height:height}} className={`${icon ? style.iconButton : ''} ${style.inner}`}>
       {icon ? <Icon size={iconSize} src={icon} plane={1} /> : ''}
@@ -12,22 +12,18 @@ export default function Button({ iconSize, icon, height="100%", text='Button', l
   )
 
   const buttonBody = (
-    <button style={size ? style[size] : {height:height}} className={`${style[size]} ${style.button}`}>{inner}</button>
+    <button onClick={onClick} style={size ? {} : {height:height}} className={`${style[size]} ${style.button}`}>{inner}</button>
   )
 
   const linkBody = (
-    <Link style={size ? style[size] :{height:height}} href={link}>
-      <a className={`${style[size]} ${style.button}`}>{inner}</a>
+    <Link style={size ? style[size] : {height:height}} href={link}>
+      <a onClick={onClick} className={`${style[size]} ${style.button}`}>{inner}</a>
     </Link>
   )
 
-  let result;
-
   switch (mode) {
-    case 'button': result = buttonBody; break;
-    case 'link': result = linkBody; break;
-    default: result = buttonBody; break;
+    case 'button': return buttonBody;
+    case 'link': return linkBody;
+    default: return buttonBody;
   }
-
-  return result;
 }
