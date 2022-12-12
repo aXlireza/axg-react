1e1b71b43e730c71f8186e322cdbdcb6ed5e2401
Axoncodes <devops@axoncodes.com>
2022-12-12 22:24:06 +0330

    modification

--- Image/index.jsx
+++ Image/index.jsx
@@ -2,7 +2,6 @@
 export default function Image({ src, sizes=[], loading='lazy', id='', alt='', customclasses='' }) {
   return (
     <axg-element
-      dev={'staging'}
       mode={'image_v1'}
       src={src}
       sizes={sizes}
