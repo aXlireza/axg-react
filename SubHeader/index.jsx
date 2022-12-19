e1ea03e283bfcbb0a6a64ad0ba106350157a26a0
Axoncodes <devops@axoncodes.com>
2022-12-19 22:06:57 +0330

    modify alignments

--- SubHeader/index.jsx
+++ SubHeader/index.jsx
@@ -3,10 +3,10 @@ import dynamic from 'next/dynamic'
 const Text = dynamic(() => import('../Text'), {ssr: false,})
 export default function SubHeader({top, mainTitle, description}) {
   return (
-    <section className='container vertical righty' style={{rowGap: '0px', backgroundColor: 'var(--tertiaryColor)'}}>
-      <Text text={top} textclasses={'nomargin'} textcolor={"var(--secondaryTextColor)"} textfontsize={"var(--standard-text-fontSize)"} />
-      <Text text={mainTitle} textclasses={'nomargin'} textcolor={"var(--primaryTextColor)"} textfontsize={"var(--xLarge-text-fontSize)"} inlineStyle={{marginTop: `-1.8%`, marginBottom: '1%'}} />
-      <Text text={description} textclasses={'nomargin'} textcolor={"var(--secondaryTextColor)"} textfontsize={"var(--smallLarge-text-fontSize)"} />
+    <section className='container vertical rtl' style={{rowGap: '0px', backgroundColor: 'var(--tertiaryColor)'}}>
+      <Text customclasses={'centerOnTablet'} text={top} textclasses={'nomargin'} textcolor={"var(--secondaryTextColor)"} textfontsize={"var(--standard-text-fontSize)"} />
+      <Text customclasses={'centerOnTablet'} text={mainTitle} textclasses={'nomargin'} textcolor={"var(--primaryTextColor)"} textfontsize={"var(--xLarge-text-fontSize)"} inlineStyle={{marginTop: `-1.8%`, marginBottom: '1%'}} />
+      <Text customclasses={'centerOnTablet'} text={description} textclasses={'nomargin'} textcolor={"var(--secondaryTextColor)"} textfontsize={"var(--smallLarge-text-fontSize)"} />
     </section>
   )
 }
