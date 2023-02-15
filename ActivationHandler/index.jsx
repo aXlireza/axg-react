db10779009f3ebe37a715d7c57cfa26fa40ea887
Axoncodes <devops@axoncodes.com>
2023-02-15 17:55:51 +0330

    simplify the actiovation handler

--- ActivationHandler/index.jsx
+++ ActivationHandler/index.jsx
@@ -1,10 +1,7 @@
 import { useEffect } from "react"
-import { loadScript } from "../../lib/script"
 
 export default function ActivationHandler() {
-  useEffect(() => {
-    loadScript(`${process.env.Axg}/layoutactivationJs`)
-    .then(() => activationHandler.init())
-  }, [])
-  return '';
+  
+  useEffect(() => activationHandlerinit(), [])
+  return <div></div>
 }
