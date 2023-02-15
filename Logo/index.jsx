5fc7d6779d84973b09766fba143bd9e52edfbc15
Axoncodes <devops@axoncodes.com>
2023-02-15 22:08:13 +0330

    update logo to take args and update with new Nextjs Link and Image upgrades

--- Logo/index.jsx
+++ Logo/index.jsx
@@ -1,13 +1,20 @@
-import Image from 'next/future/image'
+import Image from 'next/image'
 import Link from 'next/link'
 import style from './style.module.css'
 
-export default function Logo({src}) {
+export default function Logo({src, width=60, height=40}) {
   return (
-    <Link style={{display: 'block'}} href="/">
-      <a style={{display: 'block'}} className={style.headerLogo}>
-        <Image layout='responsive' width={60} height={40} src={src} alt="logo" />
-      </a>
+    <Link
+      style={{display: 'block'}}
+      href="/"
+      className={style.headerLogo}
+    >
+      <Image
+        width={width}
+        height={height}
+        src={src}
+        alt="logo"
+      />
     </Link>
   )
 }
