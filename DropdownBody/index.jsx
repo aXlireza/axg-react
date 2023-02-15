784c0b891adf4c7bea7ff6108991ab57278e6e47
Axoncodes <devops@axoncodes.com>
2023-02-15 22:08:52 +0330

    dropdown body element

---
+++ DropdownBody/index.jsx
@@ -0,0 +1,49 @@
+
+export default function Menu({
+  key,
+  dev,
+  exit,
+  headTitle,
+  headTitlecolor,
+  height,
+  color,
+  colorHover,
+  activeBackground,
+  headBackground,
+  headBackgroundHover,
+  structure,
+  fontsize,
+  title,
+  background,
+  targetLocator,
+  subOpening,
+  options,
+  optionsApi,
+  dropdownid,
+}) {
+  return (
+    <axg-element
+      key={key}
+      mode={'dropdown_body_v4'}
+      dev={dev}
+      exit={exit}
+      headTitle={headTitle}
+      headTitlecolor={headTitlecolor}
+      height={height}
+      color={color}
+      colorHover={colorHover}
+      activeBackground={activeBackground}
+      headBackground={headBackground}
+      headBackgroundHover={headBackgroundHover}
+      structure={structure}
+      fontsize={fontsize}
+      title={title}
+      background={background}
+      targetLocator={targetLocator}
+      subOpening={subOpening}
+      options={JSON.stringify(options)}
+      optionsApi={optionsApi}
+      dropdownid={dropdownid}
+    ></axg-element>
+  )
+}
