69493896f83086fc236f5fdd45564888951aa2fb
Axoncodes <devops@axoncodes.com>
2023-02-17 17:37:28 +0330

    upgraded the Logo to use axg logo componenet and set more parameters

--- Logo/index.jsx
+++ Logo/index.jsx
@@ -2,19 +2,30 @@ import Image from 'next/image'
 import Link from 'next/link'
 import style from './style.module.css'
 
-export default function Logo({src, width=60, height=40}) {
+export default function Logo({minWidth='', dev='', alt='Logo', link='/', src, width=60, height=40}) {
   return (
-    <Link
-      style={{display: 'block'}}
-      href="/"
-      className={style.headerLogo}
-    >
-      <Image
-        width={width}
-        height={height}
-        src={src}
-        alt="logo"
-      />
-    </Link>
+    // <Link
+    //   style={{display: 'block'}}
+    //   href="/"
+    //   className={style.headerLogo}
+    // >
+    //   <Image
+    //     width={width}
+    //     height={height}
+    //     src={src}
+    //     alt="logo"
+    //   />
+    // </Link>
+    <axg-element
+      mode={'logo_v3'}
+      minWidth={minWidth}
+      dev={dev}
+      link={link}
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      
+    ></axg-element>
   )
 }
