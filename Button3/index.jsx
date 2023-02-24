70ac30fb759d179edf1c09832b255ab7050176b7
Axoncodes <devops@axoncodes.com>
2023-02-24 17:40:04 +0330

    implements

---
+++ Button3/index.jsx
@@ -0,0 +1,50 @@
+
+export default function Button({
+  iconclasses,
+  plane,
+  roundlevel,
+  customclasses,
+  iconSize,
+  icon,
+  height,
+  text,
+  link,
+  bg,
+  color,
+  size,
+  mode,
+  dev,
+  id,
+  innerclasses,
+  textfontsize,
+  textclasses,
+  activecolor,
+  hovercolor,
+  hoverbg,
+  blank,
+} = {}) {
+  return <axg-element
+    mode={mode || 'button_v3'}
+    dev={dev}
+    id={id}
+    plane={plane}
+    iconsize={iconSize}
+    iconclasses={iconclasses}
+    icon={icon}
+    height={height}
+    text={text}
+    link={link}
+    bg={bg}
+    color={color}
+    size={size}
+    customclasses={customclasses}
+    roundlevel={roundlevel}
+    innerclasses={innerclasses}
+    textfontsize={textfontsize}
+    textclasses={textclasses}
+    activecolor={activecolor}
+    hovercolor={hovercolor}
+    hoverbg={hoverbg}
+    blank={blank}
+  ></axg-element>
+}
