dd2eacb190fc579f9afb3b08c3b2275dea826895
Axoncodes <devops@axoncodes.com>
2023-03-20 16:46:47 +0330

    new componenet, breadcrumb

---
+++ Breadcrumb/index.jsx
@@ -0,0 +1,18 @@
+export default function Breadcrumb({
+    dev,
+    dropdownone_head,
+    dropdownone_body,
+    dropdowntwo_head,
+    dropdowntwo_body,
+    primaryText,
+}) {
+    return (<axg-element
+        dev={dev}
+        mode={'breadcrumb_v1'}
+        dropdownone_head={JSON.stringify(dropdownone_head)}
+        dropdownone_body={JSON.stringify(dropdownone_body)}
+        dropdowntwo_head={JSON.stringify(dropdowntwo_head)}
+        dropdowntwo_body={JSON.stringify(dropdowntwo_body)}
+        primarytext={JSON.stringify(primaryText)}
+    ></axg-element>)
+}
