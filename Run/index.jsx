a4960e18a456c4f6ab5441d3a65d35aea6f0d9b6
Axoncodes <devops@axoncodes.com>
2023-03-21 23:26:59 +0330

    remove the sessions so that it'd be reuseable at each new page

--- Run/index.jsx
+++ Run/index.jsx
@@ -2,6 +2,9 @@ import { useEffect } from "react"
 
 export default function Axg() {
     useEffect(() => {
+        sessionStorage.setItem('axgRegisteredFunctionsDone', JSON.stringify([]))
+        sessionStorage.setItem('axgRegisteredFunctions', JSON.stringify([]))
+        
         setTimeout(() => {implementScripts()}, 1000)
         setTimeout(() => {implementScripts()}, 2500)
         setTimeout(() => {implementScripts()}, 3200)
